8753cf6458212a4d5d686b64ef26b11b41059dd3
sruetzler <sruetzler@arigo-software.com>
2020-03-17 20:40:52 +0100

    adapt runo-docker.sh for automation

--- scripts/run-docker.sh
+++ scripts/run-docker.sh
@@ -38,7 +38,7 @@ docker start $CONTAINER_NAME > /dev/null 2> /dev/null || {
 }
 
 if [ "$#" -eq  "0" ]; then
-	docker exec --interactive --tty $CONTAINER_NAME bash
+	docker exec --tty $CONTAINER_NAME bash
 else
-	docker exec --interactive --tty $CONTAINER_NAME $@
+	docker exec --tty $CONTAINER_NAME $@
 fi
