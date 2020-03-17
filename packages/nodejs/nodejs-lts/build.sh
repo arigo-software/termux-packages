0a83aad4a4b85425f631120f3466dbf990f1bc63
sruetzler <sruetzler@arigo-software.com>
2020-03-17 15:38:58 +0100

    build 10.14.0

--- packages/nodejs/nodejs-lts/build.sh
+++ packages/nodejs/nodejs-lts/build.sh
@@ -1,10 +1,10 @@
 TERMUX_PKG_HOMEPAGE=https://nodejs.org/
 TERMUX_PKG_DESCRIPTION="Platform built on Chrome's JavaScript runtime for easily building fast, scalable network applications"
 TERMUX_PKG_LICENSE="MIT"
-TERMUX_PKG_VERSION=10.14.0
+TERMUX_PKG_VERSION=10.16.3
 TERMUX_PKG_REVISION=2
 TERMUX_PKG_SRCURL=https://nodejs.org/dist/v${TERMUX_PKG_VERSION}/node-v${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=b05a52e556df813eb3f36c795784956b1c3c3b078cef930af6c1f2fb93642929
+TERMUX_PKG_SHA256=7bf1123d7415964775b8f81fe6ec6dd5c3c08abb42bb71dfe4409dbeeba26bbd
 # Note that we do not use a shared libuv to avoid an issue with the Android
 # linker, which does not use symbols of linked shared libraries when resolving
 # symbols on dlopen(). See https://github.com/termux/termux-packages/issues/462.
