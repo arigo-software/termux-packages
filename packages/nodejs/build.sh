669e7273dff0a9cb93321502bbca770188081246
sruetzler <sruetzler@arigo-software.com>
2020-05-06 10:39:55 +0200

    node version 10.14.0

--- packages/nodejs/build.sh
+++ packages/nodejs/build.sh
@@ -2,20 +2,18 @@ TERMUX_PKG_HOMEPAGE=https://nodejs.org/
 TERMUX_PKG_DESCRIPTION="Platform built on Chrome's JavaScript runtime for easily building fast, scalable network applications"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_VERSION=10.14.0
-TERMUX_PKG_REVISION=2
-TERMUX_PKG_SRCURL=https://nodejs.org/dist/v${TERMUX_PKG_VERSION}/node-v${TERMUX_PKG_VERSION}.tar.xz
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SHA256=b05a52e556df813eb3f36c795784956b1c3c3b078cef930af6c1f2fb93642929
+TERMUX_PKG_SRCURL=https://nodejs.org/dist/v${TERMUX_PKG_VERSION}/node-v${TERMUX_PKG_VERSION}.tar.xz
 # Note that we do not use a shared libuv to avoid an issue with the Android
 # linker, which does not use symbols of linked shared libraries when resolving
 # symbols on dlopen(). See https://github.com/termux/termux-packages/issues/462.
-TERMUX_PKG_DEPENDS="libc++, openssl, c-ares, libicu, zlib"
+TERMUX_PKG_DEPENDS="openssl, c-ares, libicu"
 TERMUX_PKG_RM_AFTER_INSTALL="lib/node_modules/npm/html lib/node_modules/npm/make.bat share/systemtap lib/dtrace"
-TERMUX_PKG_BUILD_IN_SRC=true
-TERMUX_PKG_CONFLICTS="nodejs-lts, nodejs-current"
-TERMUX_PKG_BREAKS="nodejs-dev"
-TERMUX_PKG_REPLACES="nodejs-current, nodejs-dev"
+TERMUX_PKG_BUILD_IN_SRC=yes
+TERMUX_PKG_CONFLICTS="nodejs"
 
-termux_step_configure() {
+termux_step_configure () {
 	local DEST_CPU
 	if [ $TERMUX_ARCH = "arm" ]; then
 		DEST_CPU="arm"
@@ -41,7 +39,6 @@ termux_step_configure() {
 		--dest-os=android \
 		--shared-cares \
 		--shared-openssl \
-		--shared-zlib \
 		--with-intl=system-icu \
 		--without-snapshot \
 		--cross-compiling
